fb295597173e1f3aa0c24011007fb95342843012
Adnan Khan <azuredragon1947@gmail.com>
2024-06-04 06:13:50 +0530

    popcorn_time

---
+++ src/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+export function useLocalStorage(initialState, key) {
+  const [value, setValue] = useState(function () {
+    const val = localStorage.getItem(key);
+    if (val === null) {
+      return initialState;
+    }
+    return JSON.parse(val);
+  });
+
+  useEffect(
+    function () {
+      localStorage.setItem(key, JSON.stringify(value));
+      sessionStorage.setItem(key, JSON.stringify(value));
+    },
+    [value, key]
+  );
+
+  return [value, setValue];
+}
